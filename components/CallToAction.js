3f50a48e360da88766e7e5ed0bc9eea1bffc99d0
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-04-30 16:45:53 +0100

    fix: sign up button padding

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -5,7 +5,7 @@ const CallToAction = () => (
         <h3 className="text-5xl mb-12 font-light">
             How do I get started?
         </h3>
-        <a className="w-full max-w-screen-sm px-4 py-2 uppercase bg-gray-300 text-white text-center text-3xl mx-auto">
+        <a className="w-full max-w-screen-sm py-2 uppercase bg-gray-300 text-white text-center text-3xl mx-auto">
             Click here to sign up
         </a>
     </div>
