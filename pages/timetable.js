db49f613799cea5e4a10332e1e514eb16196b0c1
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-05-19 11:02:19 +0100

    fix: timetable

--- pages/timetable.js
+++ pages/timetable.js
@@ -6,7 +6,7 @@ export default function Timetable() {
       <section className="text-gray-600 body-font">
         <div className="container px-4 py-12 mx-auto">
           <div className="flex flex-col text-center w-full mb-10">
-          <h1 className="text-5xl px-4 md:px-0 font-light tracking-wider uppercase text-center text-gray-900">
+            <h1 className="text-5xl px-4 md:px-0 font-light tracking-wider uppercase text-center text-gray-900">
               Timetable
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
@@ -73,8 +73,11 @@ export default function Timetable() {
                   </td>
                   <td className="border-t-2 border-gray-200 px-4 py-3"></td>
                   <td className="border-t-2 border-gray-200 px-4 py-3"></td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700">
-                    Kid's 5-7 years - 45 mins
+                  <td className="border-t-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-900">
+                    <span className="font-medium text-pink-700">
+                      Kid's 5-7 years - 45 mins
+                    </span>{" "}
+                    <br /> <br /> Kid's 8-10 years - 60 mins
                   </td>
                   <td className="border-t-2 border-gray-200 px-4 py-3"></td>
                 </tr>
@@ -134,18 +137,30 @@ export default function Timetable() {
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">
                     16:00pm
                   </td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700">
-                    Kid's 5-7 years - 45 mins
-                  </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-900">
                     Kid's 8-10 years - 60 mins
                   </td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700">
-                    Kid's 5-7 years - 45 mins
-                  </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-900">
                     Kid's 8-10 years - 60 mins
                   </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
+                </tr>
+                <tr>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">
+                    16:15pm
+                  </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700">
+                    Kid's 5-7 years - 45 mins
+                  </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-900"></td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-700">
+                    Kid's 5-7 years - 45 mins
+                  </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-pink-900"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
@@ -157,16 +172,14 @@ export default function Timetable() {
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-green-400">
                     Teen's Class - 60 mins
                   </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-green-400"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-green-400">
                     Teen's Class - 60 mins
                   </td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-green-400"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-green-400">
                     Teen's Class - 60 mins
                   </td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-green-400">
-                    Teen's Class - 60 mins
-                  </td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                 </tr>
@@ -214,13 +227,17 @@ export default function Timetable() {
                   </td>
                   <td className="border-t-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-gray-200 px-4 py-3 text-sm font-medium text-indigo-700">
-                    <span className="font-medium text-blue-500">Beginner's Class - 60 mins</span> <br /> <br /> Mixed Class - 90
-                    mins
+                    <span className="font-medium text-blue-500">
+                      Beginner's Class - 60 mins
+                    </span>{" "}
+                    <br /> <br /> Mixed Class - 90 mins
                   </td>
                   <td className="border-t-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-gray-200 px-4 py-3 text-sm font-medium text-indigo-700">
-                    <span className="font-medium text-blue-500">Beginner's Class - 60 mins</span> <br /> <br /> Mixed Class - 90
-                    mins
+                    <span className="font-medium text-blue-500">
+                      Beginner's Class - 60 mins
+                    </span>{" "}
+                    <br /> <br /> Mixed Class - 90 mins
                   </td>
                   <td className="border-t-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-gray-200 px-4 py-3 text-sm"></td>
