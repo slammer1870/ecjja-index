bd2e383ede7b0eb6cbb2f42a39fa189425d2e9c4
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-05-28 11:24:43 +0100

    fix: error hadling for subscribers

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,25 +1,36 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
+import mailchimp from "@mailchimp/mailchimp_marketing";
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_API_SERVER // e.g. us1
+  server: process.env.MAILCHIMP_API_SERVER, // e.g. us1
 });
 
 export default async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
-    return res.status(400).json({ error: 'Email is required' });
+    return res.status(400).json({ error: "Email is required" });
   }
 
   try {
     await mailchimp.lists.addListMember(process.env.NEWSLETTER_AUDIENCE_ID, {
       email_address: email,
-      status: 'subscribed'
+      status: "subscribed",
     });
 
-    return res.status(201).json({ error: '' });
+    return res.status(201).json({ error: "" });
   } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
+    const text = JSON.parse(error.response.text);
+    if (text.title == "Member Exists") {
+      return res
+        .status(400)
+        .json({ error: "It looks like you are already subscribed!" });
+    } else {
+      return res
+        .status(500)
+        .json({
+          error: "Oops! Something went wrong. Contact info@ecjja.com for help.",
+        });
+    }
   }
 };
