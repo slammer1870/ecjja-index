4bdeb94b14d7af0426c80b80facb91769277a322
Sam <hello@sammcnally.dev>
2021-06-21 19:50:52 +0100

    fix: email settings

--- pages/api/confirmation.js
+++ pages/api/confirmation.js
@@ -1,27 +1,27 @@
 const stripe = require("stripe")(process.env.STRIPE_SK);
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 
 export default async (req, res) => {
   const { checkout_session } = req.body;
 
-  console.log("checkout session is", checkout_session)
-
   try {
     const session = await stripe.checkout.sessions.retrieve(checkout_session);
-    const message = {
+    if (session) {
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+      const message = {
         from: "info@ecjja.com",
         to: "hello@sammcnally.dev",
-        subject: `New checkout initiated from ${email}`,
-        text: `${name} has just initiated a checkout session for the next beginner's course. Their Gi size is ${size}. You will receive a confirmation email in this thread once their payment has been approved.`,
-        replyTo: email,
+        subject: `New checkout initiated from ${session.customer_email}`,
+        text: `${session.customer_email} has just confimered their payment `,
+        replyTo: session.customer_email,
       };
-     await sgMail.send(message);
+      await sgMail.send(message);
+    }
 
-    return res.status(201).json( session );
+    return res.status(201).json(session);
   } catch (error) {
     return res.status(500).json({
-      error: error.message
+      error: error.message,
     });
   }
 };
