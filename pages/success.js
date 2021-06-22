e1e9b2c574dea3b190d3936da1dc959c213b77b7
Sam <hello@sammcnally.dev>
2021-06-22 10:58:33 +0100

    add: session id condition to success page

--- pages/success.js
+++ pages/success.js
@@ -7,28 +7,30 @@ const useSession = (session_id) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchSession = async () => {
-      setLoading(true);
-      try {
-        const res = await fetch("/api/confirmation", {
-          method: "POST",
-          body: JSON.stringify({ checkout_session: session_id }),
-          headers: {
-            "Content-type": "application/json",
-          },
-        });
+    if (session_id) {
+      const fetchSession = async () => {
+        setLoading(true);
+        try {
+          const res = await fetch("/api/confirmation", {
+            method: "POST",
+            body: JSON.stringify({ checkout_session: session_id }),
+            headers: {
+              "Content-type": "application/json",
+            },
+          });
 
-        const result = await res.json();
+          const result = await res.json();
 
-        if (session == null) {
-          setSession(result);
+          if (session == null) {
+            setSession(result);
+          }
+        } catch (error) {
+          setError(error.message);
         }
-      } catch (error) {
-        setError(error.message);
-      }
-      setLoading(false);
-    };
-    fetchSession();
+        setLoading(false);
+      };
+      fetchSession();
+    }
   }, [session_id]);
 
   return { session, loading, error };
