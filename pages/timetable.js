4df542e6b798949721b65fac98fac18ddec26b0e
Sam <hello@sammcnally.dev>
2021-07-20 07:55:51 +0100

    fix: timetable

--- pages/timetable.js
+++ pages/timetable.js
@@ -213,7 +213,7 @@ export default function Timetable() {
                   </td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm font-medium text-indigo-700">
-                    Mixed Class - 90 mins
+                    Mixed Class - 90 mins (NoGi)
                   </td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
                   <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-sm"></td>
